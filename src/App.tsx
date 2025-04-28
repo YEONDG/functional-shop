7f8073ff1b76f11717a11475014f20a810a09aa6
YDG <zzmn1234@naver.com>
2025-04-28 22:14:04 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main className='flex flex-col items-center min-h-screen bg-gray-100 space-y-5'>
+      <header className='flex justify-center gap-4 w-full p-4 bg-blue-500 text-white'>
+        <h1 className='text-5xl '>쇼핑몰</h1>
+      </header>
+    </main>
+  );
+}
+
+export default App;
